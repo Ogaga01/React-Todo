2248faab02a2ce9ab13b336cf346a49563f6af84
Ogaga <iyaraogaga69@gmail.com>
2022-12-10 13:50:13 +0100

    Add ref to  Newtask

--- src/NewTask.js
+++ src/NewTask.js
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useRef } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
 import { GrCheckbox } from 'react-icons/gr';
 import { GiCheckMark } from 'react-icons/gi';
@@ -7,6 +7,8 @@ import styles from './SASS/NewTask.module.scss';
 
 const NewTask = (props) => {
 //   const [checked, setChecked] = useState(false);
+    // const [inputvalue, setInputValue] = useState('')
+    const listItemRef = useRef()
 
   const checkHandler = () => {
     console.log('clicked');
@@ -15,35 +17,47 @@ const NewTask = (props) => {
   const deleteHandler = (id) => {
     props.tasks.filter((task) => task.id !== id);
   };
+    
+    const inputChangeHandler = (e) => {
+        if (e.key === 'Enter')
+        {
+            console.log(listItemRef.current)
+            props.onChangeInput(e.target.value)
+        }
+    }
 
   return (
     <ul className={styles.todo}>
-      {props.tasks.map((task) => (!task.checked ? (
-        <li key={task.id} className={styles.listitem}>
-          <div className={styles.state}>
-            <GrCheckbox className={styles.check} onClick={checkHandler} />
-          </div>
-          <input
-            type="text"
-            className={styles.listinput}
-            value={task.description}
-                //   onChange={}
-          />
-          <FaTrashAlt className={styles.trash} onClick={deleteHandler} />
-        </li>
-      ) : (
-        <li key={task.id} className={styles.listitem}>
-          <div className={styles.state}>
-            <GiCheckMark className={styles.active} onClick={checkHandler} />
-          </div>
-          <input
-            type="text"
-            className={`${styles.listinput} ${styles.linethrough}`}
-            value={task.description}
-          />
-          <FaTrashAlt className={styles.trash} onClick={deleteHandler} />
-        </li>
-      )))}
+      {props.tasks.map((task) =>
+        !task.checked ? (
+          <li key={task.id} className={styles.listitem} ref={listItemRef}>
+            <div className={styles.state}>
+              <GrCheckbox className={styles.check} onClick={checkHandler} />
+            </div>
+            <input
+              type="text"
+              className={styles.listinput}
+                      defaultValue={task.description}
+                      onKeyDown={inputChangeHandler}
+            //   onChange={inputChangeHandler}
+            />
+            <FaTrashAlt className={styles.trash} onClick={deleteHandler} />
+          </li>
+        ) : (
+          <li key={task.id} className={styles.listitem}>
+            <div className={styles.state}>
+              <GiCheckMark className={styles.active} onClick={checkHandler} />
+            </div>
+            <input
+              type="text"
+              className={`${styles.listinput} ${styles.linethrough}`}
+              value={task.description}
+            //   onChange={inputChangeHandler}
+            />
+            <FaTrashAlt className={styles.trash} onClick={deleteHandler} />
+          </li>
+        )
+      )}
     </ul>
   );
 };
